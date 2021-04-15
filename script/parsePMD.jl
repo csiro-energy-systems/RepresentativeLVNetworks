4db32e8228da27839b27a89c7dccd9925f79fe4c
Frederik <frederik.geth@gmail.com>
2021-04-15 15:36:16 +1000

    fix load values

--- script/parsePMD.jl
+++ script/parsePMD.jl
@@ -12,9 +12,24 @@ sols = Dict()
 for (i, casename) in case
     file = "data/"*casename*"/Master.dss"
     # cd("/Users/get050/Documents/repositories/GitHub/RepresentativeLVNetworks")
-    sols[casename] = run_mc_opf(file, ACPPowerModel, solver)
+    data = PowerModelsDistribution.parse_file(file)
+    if i ==11 # sourcebus_22000.trafo_75612178_75612178
+        for (l,load) in data["load"]
+            load["pd_nom"] /= 1.924
+            load["qd_nom"] /= 1.924
+        end
+    end
+
+    if i ==10  # sourcebus_11000.trafo_75617582_75617582
+        for (l,load) in data["load"]
+            load["pd_nom"] /= 3.235
+            load["qd_nom"] /= 3.235
+        end
+    end
+
+    sols[casename] = solve_mc_opf(data, ACPPowerModel, solver)
 end
 
 sols["D014470"]["termination_status"] == LOCALLY_SOLVED
 
-[k for (k,v) in sols if v["termination_status"] != LOCALLY_SOLVED]
+r = Dict(k=>v["termination_status"] for (k,v) in sols if v["termination_status"] != LOCALLY_SOLVED)
